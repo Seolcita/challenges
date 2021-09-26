0e4ba63a2b61d246923cdd0b33227bc77431d694
Seolcita <seolcita@gmail.com>
2021-09-25 22:39:32 -0600

    3 Accordion Card

---
+++ 3 Accordion Card/script.js
@@ -0,0 +1,9 @@
+/** @format */
+
+const toggles = document.querySelectorAll('.faq__toggle');
+
+toggles.forEach((toggle) => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active');
+  });
+});
